9e2f345f3ab833abd08092b57ddc7da915587e9f
alyssamaebb <alyssamaebb@gmail.com>
2025-04-22 17:30:20 -0400

    added .h file

---
+++ escape_game_classes.h
@@ -0,0 +1,19 @@
+// escape_game_classes.h
+
+#ifndef ADVENTURE_QUEST_CLASSES_H
+#define ADVENTURE_QUEST_CLASSES_H
+
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <map>
+#include <regex>
+
+// Forward declarations
+class Room;
+class Monster;
+class Inventory;
+class TreeNode;
+class Player;
+class Game;
+class UI;
