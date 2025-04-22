24c758dc41aa9d8efcb697032db08594086ca869
alyssamaebb <91289751+alyssamaebb@users.noreply.github.com>
2025-04-22 17:49:26 -0400

    Update escape_game_classes.h

--- escape_game_classes.h
+++ escape_game_classes.h
@@ -1,7 +1,7 @@
 // escape_game_classes.h
 
-#ifndef ADVENTURE_QUEST_CLASSES_H
-#define ADVENTURE_QUEST_CLASSES_H
+#ifndef ESCAPE_GAME_CLASSES_H
+#define ESCAPE_GAME_CLASSES_H
 
 #include <iostream>
 #include <vector>
@@ -17,3 +17,126 @@ class TreeNode;
 class Player;
 class Game;
 class UI;
+
+//---------------------------------------------
+// Inventory (Linked List)
+//---------------------------------------------
+class InventoryNode {
+public:
+    std::string itemName;
+    InventoryNode* next;
+    InventoryNode(std::string name);
+};
+
+class Inventory {
+private:
+    InventoryNode* head;
+public:
+    Inventory();
+    ~Inventory();
+    void addItem(std::string name);
+    bool removeItem(std::string name);
+    void display();
+    void sort();
+    bool search(std::string name);
+    bool regexSearch(std::string pattern);
+};
+
+//---------------------------------------------
+// Monster
+//---------------------------------------------
+class Monster {
+public:
+    std::string name;
+    int hp, attack;
+    Monster(std::string n, int h, int a);
+};
+
+//---------------------------------------------
+// Skill Tree Node
+//---------------------------------------------
+class TreeNode {
+public:
+    std::string skillName;
+    bool unlocked;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(std::string name);
+};
+
+//---------------------------------------------
+// Player
+//---------------------------------------------
+class Player {
+public:
+    int hp, attack;
+    Inventory inventory;
+    TreeNode* skillTreeRoot;
+    std::stack<std::string> battleLog;
+    std::vector<std::string> learnedSkills;
+
+    Player();
+    void learnSkill(TreeNode* root);
+    void printBattleLog();
+    void useItem(std::string name);
+};
+
+//---------------------------------------------
+// Room (Graph Node)
+//---------------------------------------------
+class Room {
+public:
+    std::string name;
+    std::string description;
+    std::vector<Room*> neighbors;
+    Monster* monster;
+
+    Room(std::string n, std::string desc);
+    void connect(Room* other);
+};
+
+//---------------------------------------------
+// UI
+//---------------------------------------------
+class UI {
+public:
+    static void displayMainMenu();
+    static int getMenuChoice();
+};
+
+//---------------------------------------------
+// Game
+//---------------------------------------------
+class Game {
+private:
+    Player player;
+    std::map<std::string, Room*> dungeonMap;
+    Room* currentRoom;
+public:
+    Game();
+    ~Game();
+    void start();
+    void mainMenu();
+    void movePlayer();
+    void initiateBattle();
+};
+
+//---------------------------------------------
+// Template Utility Functions
+//---------------------------------------------
+template <typename T>
+void swap(T& a, T& b) {
+    T temp = a;
+    a = b;
+    b = temp;
+}
+
+template <typename T>
+int search(T arr[], int size, T key) {
+    for (int i = 0; i < size; ++i) {
+        if (arr[i] == key) return i;
+    }
+    return -1;
+}
+
+#endif // ESCAPE_GAME_H
