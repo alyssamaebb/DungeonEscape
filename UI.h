ae535c1d443657ecee9e35ec1716a5ec79ea3a68
alyssamaebb <91289751+alyssamaebb@users.noreply.github.com>
2025-04-26 18:42:04 -0400

    Update UI.h

--- UI.h
+++ UI.h
@@ -3,11 +3,11 @@
 #ifndef UI_H
 #define UI_H
 
-// Handles displaying menus and gathering user input
+// Handles user interface display and input gathering
 class UI {
 public:
-    static void displayMainMenu(); // Show the main game menu
-    static int getMenuChoice(); // Get the player's menu choice
+    static void displayMainMenu(); // Show the main game meny options
+    static int getMenuChoice(); // Get the user's selected menu options
 };
 
 #endif // UI_H
