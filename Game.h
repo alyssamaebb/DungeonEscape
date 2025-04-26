e1ae3b04366ffe49dd90233298f17e52a239535d
alyssamaebb <91289751+alyssamaebb@users.noreply.github.com>
2025-04-26 18:13:10 -0400

    Create Game.h

---
+++ Game.h
@@ -0,0 +1,22 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <map>
+#include <string>
+
+class Room;
+
+class Game {
+private:
+    std::map<std::string, Room*> dungeonMap;
+    Room* currentRoom;
+public:
+    Game();
+    ~Game();
+    void movePlayer();
+    void createDungeon();
+    Room* findRoom(const std::string& roomName);
+    void displayCurrentRoom();
+};
+
+#endif // GAME_H
