04b6ed607bd283c6ef37f271251924a3b879a644
Joshieee0 <joshuaseepersaud@yahoo.com>
2025-04-27 19:08:16 -0400

    small issue fixed

--- Player.h
+++ Player.h
@@ -37,7 +37,7 @@ public:
     void displayStats();
     void setSkillTree(SkillTree& tree);
     bool canUseSkill(const std::string& skillName);
-    void useSkill(const std::string& skillName, Monster monster);
+    void useSkill(const std::string& skillName, Monster& monster);
 	SkillTree& getSkillTree() { return skillTree; } // Getter for skill tree
 
     // New methods
