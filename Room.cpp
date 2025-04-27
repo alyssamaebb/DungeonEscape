108df6461785f0353670727811437a21ac2dd8a6
Kandice <kandiceestrella@gmail.com>
2025-04-27 13:10:12 -0400

    Removed example comments.

--- Room.cpp
+++ Room.cpp
@@ -59,37 +59,3 @@ void Room::dfs(Room* currentRoom, std::unordered_set<Room*>& visited)
 		}
 	}
 }
-
-//---------------------------------------------
-// Example Room Creation in main.cpp
-//---------------------------------------------
-/*
-{
-	// Create rooms
-	Room* room1 = new Room("Entrance", "You are at the entrance of the dungeon.");
-	Room* room2 = new Room("Inventory", "Select your starting weapons.");
-	Room* room3 = new Room("Monster Lair 1", "A lair of a fearsome monster.");
-	Room* room4 = new Room("Monster Lair 2", "A lair of a fearsome monster.");
-	Room* room5 = new Room("Exit", "A hidden chamber with ancient artifacts.");
-	// Connect rooms at the start of the game
-	room1->connect(room2);
-}
-// Connect rooms in the dungeon
-	room1->connect(room3);
-*/
-/*
-//----------------------------------------------
-// Example Adding and Removing Monsters
-//----------------------------------------------
-{
-	// Create monsters
-	Monster* monster1 = new Monster("Goblin", 30, 5);
-	Monster* monster2 = new Monster("Orc", 50, 10);
-	// Set monsters in rooms
-	room3->setMonster(monster1);
-	room4->setMonster(monster2);
-	// Remove monsters from rooms
-	room3->removeMonster();
-	room4->removeMonster();
-}
-*/
