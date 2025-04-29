92d91055d845969cf5cb91c3b0c74acdd94bc45f
alyssamaebb <91289751+alyssamaebb@users.noreply.github.com>
2025-04-29 17:27:52 -0400

    change text color

--- Skill.cpp
+++ Skill.cpp
@@ -1,13 +1,16 @@
 #include "Skill.h"
 #include <iostream>
 
+#define ANSI_RESET        "\033[0m"
+#define ANSI_BLOOD_RED    "\033[91m"
+
 Skill::Skill(std::string name, int manaCost, int damage)
     : name(name), manaCost(manaCost), damage(damage), isUnlocked(false) {
 }
 
 void Skill::unlock() {
     isUnlocked = true;
-    std::cout << "You have unlocked the skill: " << name << "!\n";
+    std::cout << "You have unlocked the skill: " << ANSI_BLOOD_RED << name << ANSI_RESET << "!\n";
 }
 
 void Skill::display() const {
