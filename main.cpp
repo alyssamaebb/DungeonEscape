1b92cd559cea3a4ad44905e6661e5d9a79c405ef
Kandice <kandiceestrella@gmail.com>
2025-05-01 18:28:05 -0400

    Updated end game logic in main

--- main.cpp
+++ main.cpp
@@ -96,8 +96,6 @@ int main()
 			if (game.room4->monster == nullptr)
 			{
 				std::cout << ANSI_BLUE << "\nYou used the Goblin and Orc Keys to escape the Dungeon!" << ANSI_RESET << std::endl;
-				std::cout << "==================================================\n";
-				std::cout << "Thank you for playing Dungeon Escape Game!\n";
 				playing = false; // Exit the game
 			}
 			std::cout << "==================================================\n";
