115f2b34745b8493cafaa615e21a60ab07c4679b
alyssamaebb <91289751+alyssamaebb@users.noreply.github.com>
2025-05-01 20:10:43 -0400

    removed templates

--- escape_game_classes.h
+++ escape_game_classes.h
@@ -19,22 +19,4 @@ class Player;
 class Game;
 class UI;
 
-//---------------------------------------------
-// Template Utility Functions
-//---------------------------------------------
-template <typename T>
-void swap(T& a, T& b) {
-    T temp = a;
-    a = b;
-    b = temp;
-}
-
-template <typename T>
-int search(T arr[], int size, T key) {
-    for (int i = 0; i < size; ++i) {
-        if (arr[i] == key) return i;
-    }
-    return -1;
-}
-
 #endif // ESCAPE_GAMEC_CLASSES_H
