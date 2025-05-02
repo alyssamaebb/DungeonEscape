5769fe6465df7ca812b020cf9f43162280b1684e
alyssamaebb <91289751+alyssamaebb@users.noreply.github.com>
2025-05-02 11:05:47 -0400

    Create AnsiColors.h

---
+++ AnsiColors.h
@@ -0,0 +1,11 @@
+#ifndef ANSI_COLORS_H
+#define ANSI_COLORS_H
+
+#define ANSI_RESET        "\033[0m"
+#define ANSI_BLOOD_RED    "\033[91m"
+#define ANSI_WARNING_YELL "\033[93m"
+#define ANSI_GREY "\x1b[90m"
+#define ANSI_GREEN	"\033[92m"
+#define ANSI_BLUE	"\033[94m"
+
+#endif / / ANSI_COLORS_H
