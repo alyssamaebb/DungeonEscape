e79abf12e2b7131f3722e21704e7a2e78e089305
alyssamaebb <91289751+alyssamaebb@users.noreply.github.com>
2025-05-02 10:42:44 -0400

    lessen lines in main.cpp

--- UI.h
+++ UI.h
@@ -16,6 +16,7 @@ public:
     static void displayMainMenu(); // Show the main game meny options
     static int getMenuChoice(); // Get the user's selected menu options
     static void searchInventory(Inventory& inventory); 
+    static void displayExitMessage();
 };
 
 #endif // UI_H
